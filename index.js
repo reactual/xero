7ef4dc2714df99bf0e4a5b916253e9de8b7f3aea
Stephen Richardson <sbr464@gmail.com>
2016-06-14 16:42:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var inflect = require('inflect');
 var XERO_BASE_URL = 'https://api.xero.com';
 var XERO_API_URL = XERO_BASE_URL + '/api.xro/2.0';
 
-function Xero(key, secret, rsa_key, showXmlAttributes, {Accept: 'application/json'}) {
+function Xero(key, secret, rsa_key, showXmlAttributes, customHeaders) {
     this.key = key;
     this.secret = secret;
 
