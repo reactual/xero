f73c0ca423bb38eee41e93a733abb3b7270e8bbd
Stephen Richardson <sbr464@gmail.com>
2016-06-14 16:52:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -25,29 +25,6 @@ Xero.prototype.call = function(method, path, body, callback) {
 
     var post_body = null;
     var content_type = 'application/json';
-    // if (method && method !== 'GET' && body) {
-    //     if (Buffer.isBuffer(body)) {
-    //         post_body = body;
-    //     } else {
-    //         var root = path.match(/([^\/\?]+)/)[1];
-    //         post_body = new EasyXml({rootElement: inflect.singularize(root), rootArray: root, manifest: true}).render(body);
-    //         content_type = 'application/json';
-    //     }
-    // }
-    // var process = function(err, xml, res) {
-    //     if (err) {
-    //         return callback(err);
-    //     }
-
-    //     self.parser.parseString(xml, function(err, json) {
-    //         if (err) return callback(err);
-    //         if (json && json.Response && json.Response.Status !== 'OK') {
-    //             return callback(json, res);
-    //         } else {
-    //             return callback(null, json, res);
-    //         }
-    //     });
-    // };
     return self.oa._performSecureRequest(self.key, self.secret, method, XERO_API_URL + path, null, post_body, content_type, callback);
 }
 
