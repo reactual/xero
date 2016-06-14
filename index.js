923a2047c0fd052499b1b8e02137be1dfd4f0b0e
Stephen Richardson <sbr464@gmail.com>
2016-06-14 16:36:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function Xero(key, secret, rsa_key, showXmlAttributes, customHeaders) {
     this.key = key;
     this.secret = secret;
 
-    this.parser = new xml2js.Parser({explicitArray: false, ignoreAttrs: showXmlAttributes !== undefined ? (showXmlAttributes ? false : true) : true, async: true});
+    // this.parser = new xml2js.Parser({explicitArray: false, ignoreAttrs: showXmlAttributes !== undefined ? (showXmlAttributes ? false : true) : true, async: true});
 
     this.oa = new oauth.OAuth(null, null, key, secret, '1.0', null, "PLAINTEXT", null, customHeaders);
     this.oa._signatureMethod = "RSA-SHA1"
@@ -24,31 +24,31 @@ Xero.prototype.call = function(method, path, body, callback) {
     var self = this;
 
     var post_body = null;
-    var content_type = null;
-    if (method && method !== 'GET' && body) {
-        if (Buffer.isBuffer(body)) {
-            post_body = body;
-        } else {
-            var root = path.match(/([^\/\?]+)/)[1];
-            post_body = new EasyXml({rootElement: inflect.singularize(root), rootArray: root, manifest: true}).render(body);
-            content_type = 'application/json';
-        }
-    }
-    var process = function(err, xml, res) {
-        if (err) {
-            return callback(err);
-        }
+    var content_type = 'application/json';
+    // if (method && method !== 'GET' && body) {
+    //     if (Buffer.isBuffer(body)) {
+    //         post_body = body;
+    //     } else {
+    //         var root = path.match(/([^\/\?]+)/)[1];
+    //         post_body = new EasyXml({rootElement: inflect.singularize(root), rootArray: root, manifest: true}).render(body);
+    //         content_type = 'application/json';
+    //     }
+    // }
+    // var process = function(err, xml, res) {
+    //     if (err) {
+    //         return callback(err);
+    //     }
 
-        self.parser.parseString(xml, function(err, json) {
-            if (err) return callback(err);
-            if (json && json.Response && json.Response.Status !== 'OK') {
-                return callback(json, res);
-            } else {
-                return callback(null, json, res);
-            }
-        });
-    };
-    return self.oa._performSecureRequest(self.key, self.secret, method, XERO_API_URL + path, null, post_body, content_type, callback ? process : null);
+    //     self.parser.parseString(xml, function(err, json) {
+    //         if (err) return callback(err);
+    //         if (json && json.Response && json.Response.Status !== 'OK') {
+    //             return callback(json, res);
+    //         } else {
+    //             return callback(null, json, res);
+    //         }
+    //     });
+    // };
+    return self.oa._performSecureRequest(self.key, self.secret, method, XERO_API_URL + path, null, post_body, content_type, callback);
 }
 
 module.exports = Xero;
